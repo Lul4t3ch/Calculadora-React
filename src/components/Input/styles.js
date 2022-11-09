d9b58d67dac13633190dde509c5803ae3b91262f
Lul4t3ch <Lul4t3ch15@gmail.com>
2022-11-09 10:18:38 -0300

    add the whole project

---
+++ src/components/Input/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #aaaaff;
+    font-size: 24px;
+    font-family: "Roboto";
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #a5a5a5;
+        border: 0;
+        align: right;
+        font-size: 28px;
+        font-family: "Roboto";
+        font-weight: 700;
+        padding: 0 10px;
+        color: #000;
+
+        display: flex;
+        align-items: center;
+        justify-content: right;    
+    }
+`
